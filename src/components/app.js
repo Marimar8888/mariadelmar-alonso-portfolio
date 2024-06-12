742f6faee625a75d3e4ee20bde949e1e48fd92b2
Marimar <malonso@quadram.mobi>
2024-06-12 19:28:30 +0200

    update 12_06_24

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import moment from 'moment';
+import PorfolioContainer from './portfolio-container';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Maria del Mar Alonso Porfolio</h1>
+        <PorfolioContainer/>
+        <PorfolioContainer/>
+        <PorfolioContainer/>
+        <h2>{moment().format("MMMM Do YYYY, h:mm:ss a")}</h2>
       </div>
     );
   }
---
+++ src/components/portfolio-container.js
@@ -0,0 +1,11 @@
+import React, {Component} from "react";
+
+export default class PorfolioContainer extends Component {
+    render(){
+        return(
+            <div>
+                <h2>Porfolio items go here...</h2>
+            </div>
+        )
+    }
+}
