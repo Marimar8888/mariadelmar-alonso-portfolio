aeba8c8874f3c51a7d775a78745e25a8f44b36af
Marimar8888 <alonsomarimar@hotmail.com>
2024-07-05 12:48:57 +0200

    Solution Error in Select Value without Selection

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -8,7 +8,7 @@ export default class PortfolioForm extends Component {
         this.state = {
             name: "",
             description: "",
-            category: "",
+            category: "Website",
             position: "",
             url: "",
             thumb_image: "",
