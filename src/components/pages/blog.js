67a3ae66fa3bf9bbbaed6768e0e544365e7887c2
Marimar <malonso@quadram.mobi>
2024-07-11 11:04:34 +0200

    recepción blogs pendiente visualizar

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,14 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
-export default function() {
-  return (
-    <div>
+class Blog extends Component {
+  constructor() {
+    super();
+    this.state={
+      blogItems: []
+    }
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems(){
+    axios.get('https://alonsomarimar.devcamp.space/portfolio/portfolio_blogs', {
+      withCredentials: true
+    })
+    .then(response => {
+      this.setState({
+        blogItems: response.data.porfolio_blogs
+      });
+    })
+    .catch(error => {
+      console.log("getBlogItems error", error);
+    });
+  }
+
+  componentDidMount(){
+    this.getBlogItems();
+  }
+
+  render() {
+    return (
+      <div>
         <h2>Blog</h2>
+
         <div>
-        <Link to="/about-me">Read more about me</Link>
+          <Link to="/about-me">Read more about myself</Link>
         </div>
-        
-    </div>
-  )
+      </div>
+    );
+  }
 }
+
+export default Blog;
