096adee5069cb4d806db499c376aad6279b15cb3
Marimar <malonso@quadram.mobi>
2024-07-11 15:58:18 +0200

    scroll infinito con icono spinner

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,25 +1,54 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import BlogItem from "../blog/blog-item";
-import blogItem from "../blog/blog-item";
+
 
 class Blog extends Component {
   constructor() {
     super();
     this.state={
-      blogItems: []
+      blogItems: [],
+      totalCount: 0,
+      currentPage: 0,
+      isLoading: true
     }
     this.getBlogItems = this.getBlogItems.bind(this);
+    this.activateInfiniteScroll();
+  }
+
+  activateInfiniteScroll() {
+    window.onscroll = () => {
+      if(this.state.isLoading || this.state.blogItems.length === this.state.totalCount){
+        return;
+      }
+
+      if (
+        window.innerHeight + document.documentElement.scrollTop ===
+        document.documentElement.offsetHeight
+      ) {
+        this.setState({
+          isLoading:true
+        });
+        this.getBlogItems();
+      }
+    };
   }
 
   getBlogItems(){
-    axios.get('https://alonsomarimar.devcamp.space/portfolio/portfolio_blogs', {
+    this.setState({
+      currentPage: this.state.currentPage + 1
+    });
+
+    axios.get(`https://alonsomarimar.devcamp.space/portfolio/portfolio_blogs?page=${this.state.currentPage}`, {
       withCredentials: true
     })
     .then(response => {
       this.setState({
-        blogItems: response.data.portfolio_blogs
+        blogItems: this.state.blogItems.concat(response.data.portfolio_blogs),
+        totalCount: response.data.meta.total_records,
+        isLoading: false
       });
     })
     .catch(error => {
@@ -38,6 +67,11 @@ class Blog extends Component {
     return (
       <div className='blog-container'>
         <div className='content-container'>{blogRecords}</div>
+        {this.state.isLoading ? (
+          <div className="content-loader">
+            <FontAwesomeIcon icon="spinner" spin />
+          </div>
+        ) : null}
       </div>
     );
   }
