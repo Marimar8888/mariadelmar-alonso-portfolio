9efcc155031b95f339e3d048fc4aacf788ab4cef
Marimar <malonso@quadram.mobi>
2024-07-11 12:33:04 +0200

    renderizacion blog-detail

--- src/components/pages/blog-details.js
+++ src/components/pages/blog-details.js
@@ -1,10 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import axios from 'axios';
+import blogItem from '../blog/blog-item';
 
 export default class BlogDetails extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentId: this.props.match.params.slug,
+      blogItem: {}
+    };
+  }
+
+  getBlogItem() {
+    axios
+      .get(`https://alonsomarimar.devcamp.space/portfolio/portfolio_blogs/${this.state.currentId}`
+
+      )
+      .then(response => {
+        this.setState({
+          blogItem: response.data.portfolio_blog
+        });
+      })
+      .catch(error => {
+        console.log("getBlogItem error", error);
+      });
+  }
+
+  
+  componentDidMount() {
+    this.getBlogItem();
+  }
+
+
   render() {
+    const {
+      title,
+      content,
+      featured_image_url,
+      blog_status
+    } = this.state.blogItem;
+
     return (
       <div>
-        <h1>Blog details....</h1>
+        <h1>{title}</h1>
+
+        <img src={featured_image_url} alt="Featured" />
+        <div>
+          {content}
+        </div>
       </div>
     )
   }
