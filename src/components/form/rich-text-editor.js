00198c8479fb512a46d9ee76414b16d4ead48f20
Marimar <malonso@quadram.mobi>
2024-07-16 13:15:44 +0200

    inclustar una imagen en un texto con editor texto enriquecido

--- src/components/form/rich-text-editor.js
+++ src/components/form/rich-text-editor.js
@@ -3,6 +3,8 @@ import { EditorState, convertToRaw } from "draft-js";
 import { Editor } from "react-draft-wysiwyg";
 import draftToHtml from "draftjs-to-html";
 import htmlToDraft from "html-to-draftjs";
+import { resolve } from "path";
+import { rejects } from "assert";
 
 export default class RichTextEditor extends Component {
 
@@ -13,7 +15,10 @@ export default class RichTextEditor extends Component {
             editorState: EditorState.createEmpty()
         };
         this.onEditorStateChange = this.onEditorStateChange.bind(this);
+        this.getBase64 = this.getBase64.bind(this);
+        this.uploadFile = this.uploadFile.bind(this);
     }
+
 
     onEditorStateChange(editorState) {
         this.setState(
@@ -26,6 +31,20 @@ export default class RichTextEditor extends Component {
         );
     }
 
+    
+    getBase64(file, callback) {
+        let reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = () => callback(reader.result);
+        reader.onerror = error => {};
+    }
+
+    uploadFile(file) {
+        return new Promise((resolve, reject) => {
+          this.getBase64(file, data => resolve({ data: { link: data } }));
+        });
+    }
+
     render() {
 
         return (
@@ -35,6 +54,19 @@ export default class RichTextEditor extends Component {
                     wrapperClassName='demo-wrapper'
                     editorClassName='demo-editor'
                     onEditorStateChange={this.onEditorStateChange}
+                    toolbar = {{
+                        inline: { inDropdown: true },
+                        list: { inDropdown: true },
+                        textAlign: { inDropdown: true },
+                        link: { inDropdown: true },
+                        history: { inDropdown: true },
+                        image: {
+                            uploadCallback: this.uploadFile,
+                            alt: { present: true, mandatory: false },
+                            previewImage: true,
+                            inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
+                        }
+                    }}
                 />
             </div>
         )
