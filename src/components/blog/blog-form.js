f08053ad5e3077bcdd1afd6c935c19baf390108d
Marimar <malonso@quadram.mobi>
2024-07-18 10:56:56 +0200

    actualizar blog-form dependiendo si estoy en edición o no

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -13,7 +13,9 @@ export default class BlogForm extends Component {
             title: "",
             blog_status: "",
             content: "",
-            featured_image: ""
+            featured_image: "",
+            apiUrl: "",
+            apiAction: "post"
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -48,11 +50,13 @@ export default class BlogForm extends Component {
             this.setState({
                 id: this.props.blog.id,
                 title: this.props.blog.title,
-                status: this.props.blog.status
+                blog_status: this.props.blog.blog_status,
+                content: this.props.blog.content,
+                apiUrl: `https://alonsomarimar.devcamp.space/portfolio/portfolio_blogs/${this.props.blog.id}`,
+                apiAction: "patch"
             });
         }
     }
-
     componentConfig() {
         return {
             iconFiletypes: [".jpg", ".png"],
@@ -96,12 +100,12 @@ export default class BlogForm extends Component {
     }
 
     handleSubmit(event) {
-        axios
-            .post(
-                "https://alonsomarimar.devcamp.space/portfolio/portfolio_blogs",
-                this.buildForm(),
-                { withCredentials: true }
-            )
+        axios({
+            method: this.state.apiAction,
+            url: this.state.apiUrl,
+            data: this.buildForm(),
+            withCredentials: true
+        })
             .then(response => {
                 if (this.state.featured_image) {
                     this.featuredImageRef.current.dropzone.removeAllFiles();
@@ -114,9 +118,14 @@ export default class BlogForm extends Component {
                     featured_image: ""
                 });
 
-                this.props.handleSuccessfullFormSubmission(
-                    response.data.portfolio_blog
-                );
+                if (this.props.editMode) {
+                    this.props.handleUpdateFormSubmission(
+                        response.data.portfolio_blog);
+                } else {
+                    this.props.handleSuccessfullFormSubmission(
+                        response.data.portfolio_blog
+                    );
+                }
             })
             .catch(error => {
                 console.log("handleSubmit for blog error", error);
