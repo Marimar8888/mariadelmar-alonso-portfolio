60869f7bc6de5289918cffe290dfd034b905e2e7
Marimar <malonso@quadram.mobi>
2024-07-18 16:38:07 +0200

    mejorar el filtrado del home

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -17,47 +17,89 @@ export default class PortfolioContainer extends Component {
     this.getPortfolioItems();
   }
 
-  getPortfolioItems() {
-    axios.get('https://alonsomarimar.devcamp.space/portfolio/portfolio_items')
-      .then(response => {
-        this.setState({
-          data: response.data.portfolio_items, // Asegúrate de que el nombre de la propiedad es correcto
-          isLoading: false
+    getPortfolioItems(filter = null) {
+      axios.get('https://alonsomarimar.devcamp.space/portfolio/portfolio_items')
+        .then(response => {
+          if(filter){
+            this.setState({
+              data: response.data.portfolio_items.filter(item => {
+                return item.category === filter;
+              })
+            });
+          }else{
+            this.setState({
+              data: response.data.portfolio_items, // Asegúrate de que el nombre de la propiedad es correcto
+              isLoading: false
+            });
+          }
+        })
+        .catch(err => {
+          console.log(err);
+          this.setState({ isLoading: false }); // Maneja el estado de carga en caso de error
         });
-      })
-      .catch(err => {
-        console.log(err);
-        this.setState({ isLoading: false }); // Maneja el estado de carga en caso de error
-      });
-  }
-
-  handleFilter(filter) {
-    this.setState({
-      data: this.state.data.filter(item => {
-        return item.category === filter;
-      })
-    });
-  }
-
-  portfolioItems() {
-    return this.state.data.map(item => {
-      return <PortfolioItem key={item.id} item={item} />;
-    });
-  }
-
-  render() {
-    if(this.state.isLoading) {
-      return <div>Loading...</div>;
     }
 
-    return (
-      <div className="portfolio-items-wrapper"> 
-        <button className="btn" onClick={() => this.handleFilter("Website")}>eCommerce</button>
-        <button className="btn" onClick={() => this.handleFilter("Tecnology")}>Scheduling</button>
-        <button className="btn" onClick={() => this.handleFilter("Education")}>Enterprise</button>
-        {this.portfolioItems()} 
-      </div>
-       
-    );
+    handleFilter(filter) {
+      if (filter === "CLEAR_FILTERS") {
+        this.getPortfolioItems();
+      } else {
+        this.getPortfolioItems(filter);
+      }
+    }
+
+    portfolioItems() {
+      return this.state.data.map(item => {
+        return <PortfolioItem key={item.id} item={item} />;
+      });
+    }
+
+    render() {
+      if (this.state.isLoading) {
+        return <div>Loading...</div>;
+      }
+
+      return (
+        <div className="homepage-wrapper">
+          <div className="filter-links">
+            <button
+              className="btn"
+              onClick={() => this.handleFilter("Education")}
+            >
+              Education
+            </button>
+            <button
+              className="btn"
+              onClick={() => this.handleFilter("Tecnology")}
+            >
+              Tecnology
+            </button>
+            <button
+              className="btn"
+              onClick={() => this.handleFilter("Website")}
+            >
+              Website
+            </button>
+            <button
+              className="btn"
+              onClick={() => this.handleFilter("Business Application")}
+            >
+              Business Application
+            </button>
+            <button
+              className="btn"
+              onClick={() => this.handleFilter("Social Media")}
+            >
+              Social Media
+            </button>
+            <button
+              className="btn"
+              onClick={() => this.handleFilter("CLEAR_FILTERS")}
+            >
+              All
+            </button>
+          </div>
+          <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
+        </div>
+      );
+    }
   }
-}
