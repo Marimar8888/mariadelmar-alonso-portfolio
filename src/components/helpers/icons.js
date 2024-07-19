0d19c98c7b400fd6c3c2fee97df9a93c9bddf034
Marimar <malonso@quadram.mobi>
2024-07-19 09:42:53 +0200

    styles in auth

--- src/components/helpers/icons.js
+++ src/components/helpers/icons.js
@@ -2,13 +2,14 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import {
     faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone,
     faEnvelope,
-    faMapMarkedAlt
+    faMapMarkedAlt,
+    faLock
 } from '@fortawesome/free-solid-svg-icons';
 
 const Icons = () => {
 
-    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone,
-        faEnvelope, faMapMarkedAlt);
+    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle, faPhone, 
+        faEnvelope, faMapMarkedAlt, faLock);
 
 }
 
